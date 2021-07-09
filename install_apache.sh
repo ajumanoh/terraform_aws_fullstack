e6bc7ac4453c239d3807dd99c98cb39d08368749
ajumanoh <ajumano@gmail.com>
2021-07-08 21:29:27 -0400

    Updated Jenkins file

--- install_apache.sh
+++ install_apache.sh
@@ -4,7 +4,7 @@ sudo yum -y update
 echo "Install Apache"
 sudo yum -y install httpd
 
+echo "Hello from ip - $(hostname -I)"  > /var/www/html/index.html
+
 echo "Start Apache Server"
 sudo service httpd start  
-
-echo "Hello from ip - $(hostname -f)"  > /var/www/html/index.html
